b140c2cde3901f3d887bb0f2e401c2a2c6b488eb
PimaCoolCode <pima@coolcode.io>
2026-05-08 18:18:17 +0200

    Started Game

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -1,6 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject food;
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(food, shootPoint.position, Random.rotation);
+        }
+    }
 }
